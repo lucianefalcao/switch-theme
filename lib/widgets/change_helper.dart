9ec2e48b99edcabc993f354dca1b8bd5cab2a9ba
Luciane Falcão <36483575+lucianefalcao@users.noreply.github.com>
2020-06-14 13:04:33 -0300

    Delete change_helper.dart

--- lib/widgets/change_helper.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:switch_theme/change_theme.dart';
-
-void change(BuildContext context, ThemeData themeData) {
-    ChangeTheme.of(context).changeTheme(themeData);
-}
